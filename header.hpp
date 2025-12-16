8a147f95d52df1ba70bde016aaf94ca0982dd192
Luigi_Iozzino <luifraste@gmail.com>
2025-12-16 01:09:58 +0100

    check

--- header.hpp
+++ header.hpp
@@ -6,7 +6,7 @@ class oggetto {
     string nome;
     float peso;
     string materiale; //legno,ferro,tungsteno,vetro(pozione)
-    public:
+    public://check
         oggetto(){};
         oggetto(string nome,float peso,string materiale){
             this->nome=nome;
